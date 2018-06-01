2e9d24fa42542a0084c3a0b1915e0bd04feea085
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-06-01 14:59:51 +0200

    Create test for check response object in SecretCreateService.

--- tests/domain/Services/SecretCreateService/SecretCreateServiceTest.php
+++ tests/domain/Services/SecretCreateService/SecretCreateServiceTest.php
@@ -2,6 +2,8 @@
 
 namespace App\tests\domain\Services;
 
+
+use App\domain\Entities\Secret\Secret;
 use App\domain\Entities\Secret\SecretFactoryImp;
 use App\domain\Services\SecretCreateService\SecretCreateService;
 use App\domain\Services\SecretCreateService\SecretCreateServiceRequest;
@@ -10,34 +12,101 @@ use App\domain\Services\ServiceResponse;
 use App\domain\ValueObjects\LinkForShare\LinkForShareFactoryImp;
 use App\domain\ValueObjects\SecretId\SecretIdFactory;
 use PHPUnit\Framework\TestCase;
-use Ramsey\Uuid\Uuid;
-
 
 class SecretCreateServiceTest extends TestCase
 {
-    public function testServiceMustReturnAServiceResponse()
+    private $service;
+    private $request;
+    private $response;
+
+    public function setUp()
     {
-        // Arrange
-        $secretFactory = new SecretFactoryImp();
-        $linkForShareFactory = new LinkForShareFactoryImp();
-        $service = new SecretCreateService($secretFactory, $linkForShareFactory);
-        $identifier = Uuid::uuid4();
+        $this->buildRequest();
+        $this->buildService();
+        $this->executeService();
+    }
+
+    private function buildRequest(): void
+    {
+        $identifier = '1234';
         $secretId = SecretIdFactory::create($identifier);
         $message = 'This is a secret.';
-        $protocol = 'http';
-        $domain = 'thisisatest.com';
-        $secretCreateServiceRequest = new SecretCreateServiceRequest();
-        $secretCreateServiceRequest->setSecretId($secretId);
-        $secretCreateServiceRequest->setMessage($message);
-        $secretCreateServiceRequest->setProtocol($protocol);
-        $secretCreateServiceRequest->setDomain($domain);
+        $protocol = 'https';
+        $domain = 'sharedsecrets.eltortuganegra.com';
+        $this->request = new SecretCreateServiceRequest();
+        $this->request->setSecretId($secretId);
+        $this->request->setMessage($message);
+        $this->request->setProtocol($protocol);
+        $this->request->setDomain($domain);
+    }
 
-        $secretCreateServiceResponse = $service->execute($secretCreateServiceRequest);
+    private function buildService(): void
+    {
+        $secretFactory = new SecretFactoryImp();
+        $linkForShareFactory = new LinkForShareFactoryImp();
+        $this->service = new SecretCreateService($secretFactory, $linkForShareFactory);
+    }
 
+    private function executeService()
+    {
+        $this->response = $this->service->execute($this->request);
+    }
+
+    public function testServiceMustReturnAServiceResponse()
+    {
         // Act
-        $isAServiceResponse = $secretCreateServiceResponse instanceof ServiceResponse;
+        $isAServiceResponse = $this->response instanceof ServiceResponse;
 
         // Assert
         $this->assertEquals(true, $isAServiceResponse);
     }
+
+    public function testResponseMustContentTheCreatedSecret()
+    {
+        // Arrange
+        $secret = $this->response->getSecret();
+
+        // Act
+        $isInstanceOfSecret = $secret instanceof Secret;
+
+        // Assert
+        $this->assertEquals(true, $isInstanceOfSecret);
+    }
+
+    public function testResponseMustContentSecretIdOfTheCreatedSecret()
+    {
+        // Arrange
+        $secret = $this->response->getSecret();
+
+        // Act
+        $secretId = $secret->getSecretId();
+
+        // Assert
+        $this->assertEquals('1234', $secretId->getIdentifier());
+    }
+
+    public function testResponseMustContentMessageOfTheCreatedSecret()
+    {
+        // Arrange
+        $secret = $this->response->getSecret();
+
+        // Act
+        $message = $secret->getMessage();
+
+        // Assert
+        $this->assertEquals('This is a secret.', $message);
+    }
+
+    public function testResponseMustContentLinkForShareOfTheCreatedSecret()
+    {
+        // Arrange
+        $linkForShare = $this->response->getLinkForShare();
+
+        // Act
+        $url = $linkForShare->getUrl();
+
+        // Assert
+        $this->assertEquals('https://sharedsecrets.eltortuganegra.com/secret/1234', $url);
+    }
+
 }
