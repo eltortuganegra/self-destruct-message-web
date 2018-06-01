4fcc77b458f58e456fdb51fc8f27702604113c63
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-06-01 14:01:35 +0200

    Create test for LinkForShare

--- src/domain/ValueObjects/LinkForShare/LinkForShareFactory.php
+++ src/domain/ValueObjects/LinkForShare/LinkForShareFactory.php
@@ -5,7 +5,7 @@ namespace App\domain\ValueObjects\LinkForShare;
 
 class LinkForShareFactory
 {
-    static public function create(string $protocol, string $domain, string $identifier): LinkForShare
+    public function create(string $protocol, string $domain, string $identifier): LinkForShare
     {
         return new LinkForShareImp($protocol, $domain, $identifier);
     }
---
+++ tests/domain/ValueObjects/LinkForShare/LinkForShareTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\domain\ValueObjects\LinkForShare\LinkForShareFactory;
+use PHPUnit\Framework\TestCase;
+
+class LinkForShareTest extends TestCase
+{
+
+    public function testLinkForShareMustReturnUrl()
+    {
+        // Arrange
+        $protocol = 'https';
+        $domain = 'amazingdomain.com';
+        $identifier = '1234';
+        $linkForShareFactory = new LinkForShareFactory();
+        $linkForShare = $linkForShareFactory->create($protocol, $domain, $identifier);
+
+        // Act
+        $url = $linkForShare->getUrl();
+
+        // Assert
+        $this->assertEquals('https://amazingdomain.com/secret/1234', $url);
+    }
+
+}
