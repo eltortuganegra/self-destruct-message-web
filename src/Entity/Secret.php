8cb86b7bdad5acec4be7ea36697d7616e582afdd
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-07-02 19:35:39 +0200

    When users paste a link for share this link must show the secret #10

---
+++ src/Entity/Secret.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SecretRepository")
+ */
+class Secret
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $secretId;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $message;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getSecretId(): ?string
+    {
+        return $this->secretId;
+    }
+
+    public function setSecretId(string $secretId): self
+    {
+        $this->secretId = $secretId;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
