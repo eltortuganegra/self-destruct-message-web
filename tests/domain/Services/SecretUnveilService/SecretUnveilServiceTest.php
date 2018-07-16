9450e43277297694a2763807c92f8f7b915dc332
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-07-16 18:14:54 +0200

    Refactor. SecretShowController must be renamed to SecretUnveilController #49

--- tests/domain/Services/SecretUnveilService/SecretUnveilServiceTest.php
+++ tests/domain/Services/SecretUnveilService/SecretUnveilServiceTest.php
@@ -1,8 +1,7 @@
 <?php
 
 use App\domain\Entities\EntitiesFactory;
-use App\domain\Entities\Secret\SecretFactoryImp;
-use App\domain\Infrastructure\Repositories\MemorySecretRepository;
+use App\domain\Infrastructure\Repositories\RepositoriesFactory;
 use App\domain\Services\SecretShowAndDestroyService\SecretUnveilServiceRequest;
 use App\domain\Services\ServicesFactory;
 use App\domain\ValueObjects\ValueObjectsFactory;
@@ -16,28 +15,79 @@ class SecretUnveilServiceTest extends TestCase
 
     public function setUp()
     {
-        $identifier = Ramsey\Uuid\Uuid::uuid4();
-        $secretIdFactory = ValueObjectsFactory::getSecretIdFactory();
-        $secretId = $secretIdFactory->create($identifier);
-        $secretFactory = EntitiesFactory::getSecretFactory();
+        $this->loadSecretRepository();
+        $this->createDefaultSecret();
+        $this->addSecretToTheRepository();
+        $serviceRequest = $this->buildServiceRequest();
+        $service = $this->buildSecretUnveilService();
+        $this->serviceResponse = $service->execute($serviceRequest);
+    }
+
+    private function createDefaultSecret(): void
+    {
+        $secretId = $this->generateSecretId();
+        $message = $this->generateMessage();
+        $expirationTime = $this->generateExpirationTime();
+        $this->generateSecret($secretId, $message, $expirationTime);
+    }
+
+    private function loadSecretRepository(): void
+    {
+        $this->secretRepository = RepositoriesFactory::getMemorySecretRepository();
+    }
+
+    private function generateSecretId()
+    {
+        $secretId = $this->secretRepository->nextIdentity();
+
+        return $secretId;
+    }
+
+    private function generateMessage(): \App\domain\ValueObjects\Message\Message
+    {
         $messageText = 'This is awesome secret message.';
         $messageFactory = ValueObjectsFactory::getMessageFactory();
         $message = $messageFactory->create($messageText);
+
+        return $message;
+    }
+
+    private function generateExpirationTime(): \App\domain\ValueObjects\ExpirationTime\ExpirationTime
+    {
         $expirationTimeFactory = ValueObjectsFactory::getExpirationTimeFactory();
         $expirationTimeInSeconds = 120;
         $expirationTime = $expirationTimeFactory->create($expirationTimeInSeconds);
+
+        return $expirationTime;
+    }
+
+    private function generateSecret($secretId, $message, $expirationTime): void
+    {
+        $secretFactory = EntitiesFactory::getSecretFactory();
         $this->secret = $secretFactory->create($secretId, $message, $expirationTime);
+    }
 
-        $this->secretRepository = new MemorySecretRepository($secretIdFactory);
+    private function addSecretToTheRepository(): void
+    {
         $this->secretRepository->add($this->secret);
+    }
 
+    private function buildServiceRequest()
+    {
+        $secretIdFactory = ValueObjectsFactory::getSecretIdFactory();
         $serviceRequest = new SecretUnveilServiceRequest($secretIdFactory);
-        $serviceRequest->setIdentifier($identifier);
+        $serviceRequest->setIdentifier($this->secret->getSecretId()->getIdentifier());
         $serviceRequest->setDomain('test.com');
         $serviceRequest->setProtocol('https');
 
+        return $serviceRequest;
+    }
+
+    private function buildSecretUnveilService(): \App\domain\Services\Service
+    {
         $service = ServicesFactory::createSecretUnveilService($this->secretRepository);
-        $this->serviceResponse = $service->execute($serviceRequest);
+
+        return $service;
     }
 
     public function testServiceMustReturnSecret()
