e0bc7d7982ee77dfaa9160e890a0f91faa78bf35
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-07-20 07:33:17 +0200

    When users unveil an expired secret they must get an error #33

--- src/domain/Services/SecretUnveilService/SecretUnveilServiceImp.php
+++ src/domain/Services/SecretUnveilService/SecretUnveilServiceImp.php
@@ -6,6 +6,7 @@ use App\domain\Infrastructure\Repositories\SecretRepository;
 use App\domain\Services\Service;
 use App\domain\Services\ServiceRequest;
 use App\domain\Services\ServiceResponse;
+use App\domain\ValueObjects\LinkForShare\LinkForShare;
 use App\domain\ValueObjects\LinkForShare\LinkForShareFactory;
 use DateTime;
 
@@ -13,6 +14,7 @@ class SecretUnveilServiceImp implements Service, SecretUnveilService
 {
     private $secretRepository;
     private $linkForShareFactory;
+    private $secret;
 
     public function __construct(SecretRepository $secretRepository, LinkForShareFactory $linkForShareFactory)
     {
@@ -22,25 +24,56 @@ class SecretUnveilServiceImp implements Service, SecretUnveilService
 
     public function execute(ServiceRequest $serviceRequest): ServiceResponse
     {
-        $secretId = $serviceRequest->getSecretId();
-        $secret = $this->secretRepository->findBySecretId($secretId);
+        $this->findSecretInRepository($serviceRequest);
+        $this->validations();
+        $this->removeSecretFromRepository();
 
-        if (empty($secret)) {
+        return $this->builtResponse($serviceRequest);
+    }
+
+    private function validations(): void
+    {
+        if (empty($this->secret)) {
             throw new SecretNotFoundException('WTF');
         }
-        if ($secret->getExpirationDate() < new DateTime() ) {
+        if ($this->isSecretExpired()) {
             throw new ExpirationTimeIsExpiredException();
         }
+    }
 
+    private function isSecretExpired(): bool
+    {
+        return $this->secret->getExpirationDate() < new DateTime();
+    }
+
+    private function builtLinkForShare(ServiceRequest $serviceRequest): LinkForShare
+    {
         $linkForShare = $this->linkForShareFactory->create(
             $serviceRequest->getProtocol(),
             $serviceRequest->getDomain(),
             $serviceRequest->getIdentifier()
         );
 
-        $this->secretRepository->remove($secret);
+        return $linkForShare;
+    }
 
-        return new SecretUnveilResponse($secret, $linkForShare);
+
+    private function findSecretInRepository(ServiceRequest $serviceRequest): void
+    {
+        $secretId = $serviceRequest->getSecretId();
+        $this->secret = $this->secretRepository->findBySecretId($secretId);
+    }
+
+    private function removeSecretFromRepository(): void
+    {
+        $this->secretRepository->remove($this->secret);
+    }
+
+    private function builtResponse(ServiceRequest $serviceRequest): SecretUnveilResponse
+    {
+        $linkForShare = $this->builtLinkForShare($serviceRequest);
+
+        return new SecretUnveilResponse($this->secret, $linkForShare);
     }
 
 }
